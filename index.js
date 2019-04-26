3da5a6b61183ffaa8e0be875ed62f13b4be06652
Rachel <racmocon@gmail.com>
2019-04-26 09:55:02 -0400

    Removed duplicate rules from config, and changed defaultSeverity

--- index.js
+++ index.js
@@ -6,18 +6,10 @@
 
 module.exports = {
   extends: 'stylelint-config-standard',
-  defaultSeverity: 'warning',
+  defaultSeverity: 'error',
   rules: {
-    'block-closing-brace-newline-after': 'always',
-    'block-opening-brace-newline-after': 'always',
-    'block-closing-brace-newline-before': 'always',
-    'block-opening-brace-space-before': 'always',
-    'color-hex-case': 'lower',
     'declaration-block-semicolon-newline-after': 'always',
-    'declaration-colon-space-after': 'always-single-line',
-    'indentation': 2,
     'number-leading-zero': 'never',
-    'rule-empty-line-before': ['always', { 'except': ['first-nested'] }],
-    'selector-list-comma-newline-after': 'always'
+    'rule-empty-line-before': ['always', { 'except': ['first-nested'] }]
   }
 };
